7b6b9355885d3ddb72cec2c150df79351060d2fb
jatinnegi <negijatinn@gmail.com>
2024-02-25 09:23:53 +0530

    Setup landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,13 @@
-import { useState } from "react";
-import {
-  CssBaseline,
-  Container,
-  Typography,
-  Checkbox,
-  FormControlLabel,
-  FormControl,
-} from "@mui/material";
-import { ThemeProvider } from "@emotion/react";
-import { darkTheme, lightTheme } from "@/theme.tsx";
+import { Box } from "@mui/material";
+import Container from "@/components/Container";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState<boolean>(false);
-
-  const theme = darkMode ? darkTheme : lightTheme;
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Container maxWidth="lg">
-        <FormControl style={{ userSelect: "none" }}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={darkMode}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  setDarkMode(e.target.checked);
-                }}
-              />
-            }
-            label="Dark Mode"
-          />
-        </FormControl>
-        <Typography variant="h5">Material UI + Typescript</Typography>
-      </Container>
-    </ThemeProvider>
+    <Container>
+      <Box component="div" sx={{ display: "flex" }}>
+        <Box component="div" sx={{ bgcolor: "red", width: "60%" }}></Box>
+        <Box component="div" sx={{ bgcolor: "blue", width: "40%" }}></Box>
+      </Box>
+    </Container>
   );
 }
