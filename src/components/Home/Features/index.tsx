13599dd2fe41f951f5c528743288791b57afa99f
jatinnegi <negijatinn@gmail.com>
2024-03-01 23:25:54 +0530

    Remove unused dependencies

--- src/components/Home/Features/index.tsx
+++ src/components/Home/Features/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect } from "react";
+import { FC, useState } from "react";
 import { Box } from "@mui/material";
 import Section from "./Section";
 import FeaturesBackground from "./FeaturesBackground";
