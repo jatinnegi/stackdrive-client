04e3585e694c5a4321ed06038515cc07bfb3395b
jatinnegi <negijatinn@gmail.com>
2026-03-11 00:53:16 +0530

    Update proxy target server domain

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://stackdrive-express-js-server.onrender.com/",
         changeOrigin: true,
       },
     },
